e1379a34eca5ad02c5cb679a95e63c5cc8b8a419
mahawi1992 <martinhgwilliams@gmail.com>
2024-12-08 17:24:42 -0800

    fix: Update Dockerfile with proper configuration and permissions

--- letta/server/startup.sh
+++ letta/server/startup.sh
@@ -7,11 +7,10 @@ if [ -n "$LETTA_PG_URI" ] || [ -n "$LETTA_PG_DB" ]; then
     alembic upgrade head
 fi
 
-if [ "$MEMGPT_ENVIRONMENT" = "DEVELOPMENT" ]; then
+if [ "$LETTA_ENVIRONMENT" = "DEVELOPMENT" ]; then
     echo "Starting in development mode!"
     uvicorn letta.server.rest_api.app:app --reload --reload-dir /letta --host 0.0.0.0 --port 8283
 else
-    # Production start command here (replace with the actual production command)
     echo "Starting in production mode!"
     uvicorn letta.server.rest_api.app:app --host 0.0.0.0 --port 8283
 fi
